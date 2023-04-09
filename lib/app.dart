8c69a12e159f5a4fac644101d1465c5da733c4d3
todoggler <kentokuraCS@gmail.com>
2023-04-09 12:38:51 +0900

    refactor: ファイル分割

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:timeline/my_home_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
